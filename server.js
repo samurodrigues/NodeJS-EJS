982d5ada9951eac394ab68f2fd73f05fa1b1f40d
Samuel S Rodrigues <samuelstievenrodrigues@gmail.com>
2023-01-22 16:44:17 -0300

    server created

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express() //Instanciando
+
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+  res.render('index')
+})
+
+app.get('/sobre', function (req, res) {
+  res.render('about')
+})
+
+app.listen(8080)
